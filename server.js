f96cae6be3576a7220c24e775dd5d16462179808
GregoireEpsi <gregoire.barre@epsi.fr>
2021-03-10 09:39:30 +0100

    fix bug

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const http = require('http')
 const cors = require("cors");
 const dbMysql = require("./app/config/mysql.config.js")
 const connectionBdd = require("./app/services/ConnectionBdd")
-var passwordHash = require('password-hash');
-const jwt = require('jsonwebtoken');
-var randomstring = require("randomstring");
 const auth = require("./app/middleware/auth.js");
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
+
+
+
+
 
 const app = express();
-
-
+http.createServer(app).listen(8081)
+console.log("Listening at:// port:%s (HTTP)", 8081)
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 // init connection to bdd
-
 connectionBdd.connectionBdd()
 
-
 app.use(cors())
-
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //#region Add Disquette
@@ -31,9 +31,9 @@ app.post("/disquette", (req, res) => {
         req.body.idAutor = req.body.userId
         dbMysql.dbMysql.query('INSERT INTO disquette SET content = ? , idAutor =? , isValid = 0', [req.body.content, req.body.idAutor], function (error, results, fields) {
             if (error) throw error;
-            res.end("OK");
+            res.status(200).json({ message: "disquette ajouté" })
         });
-    } catch (error) { res.end("NOK") }
+    } catch (error) { res.status(500).json({ message: "unknown error" }) }
 });
 //#endregion
 
@@ -43,9 +43,9 @@ app.get("/getAllDisquette", (req, res) => {
         dbMysql.dbMysql.query("SELECT * FROM disquette", function (err, result) {
             if (err) throw err;
             console.log(result);
-            res.end(JSON.stringify(result))
+            res.status(200).json(result)
         });
-    } catch { console.log(error) }
+    } catch { res.status(500).json({ message: "unknown error" }) }
 });
 //#endregion
 
@@ -55,9 +55,9 @@ app.post("/favori", (req, res) => {
         req.body.idUser = req.body.userId;
         dbMysql.dbMysql.query('INSERT INTO favori SET idUser=? , idDisquette=?', [req.body.idUser, req.body.idDisquette], function (error, results, fields) {
             if (error) throw error;
-            res.end("Favori ajouté");
+            res.status(200).json({ message: "Disquette liké" })
         });
-    } catch { console.log(error) }
+    } catch { res.status(500).json({ message: "unknown error" }) }
 });
 //#endregion
 
@@ -65,12 +65,12 @@ app.post("/favori", (req, res) => {
 app.get("/favori/:userId", (req, res) => {
     try {
         req.params.idUser = req.params.userId
-        dbMysql.dbMysql.query("SELECT * FROM favori WHERE idUser=?", [req.params.idUser], function (err, result) {
+        dbMysql.dbMysql.query("SELECT content , idDisquette from favori join disquette ON favori.idDisquette = disquette.id WHERE idUser = ?", [req.params.idUser], function (err, result) {
             if (err) throw err;
             console.log(result);
-            res.end(JSON.stringify(result))
+            res.status(200).json(result)
         });
-    } catch { console.log(error) }
+    } catch { res.status(500).json({ message: "unknown error" }) }
 });
 //#endregion
 
@@ -81,9 +81,9 @@ app.delete('/favori', function (req, res) {
         console.log(req.body.userId, req.body.idDisquette);
         dbMysql.dbMysql.query('DELETE FROM favori WHERE idUser=? and idDisquette=?', [req.body.idUser, req.body.idDisquette], function (error, results, fields) {
             if (error) throw error;
-            res.end("Favori supprimer");
+            res.status(200).json({ message: "Like supprimé" })
         });
-    } catch { console.log(error) }
+    } catch { res.status(500).json({ message: "unknown error" }) }
 });
 //#endregion
 
@@ -93,9 +93,9 @@ app.get("/getAllUser", (req, res) => {
         dbMysql.dbMysql.query("SELECT * FROM user where isAdmin=0", function (err, result) {
             if (err) throw err;
             console.log(result);
-            res.end(JSON.stringify(result))
+            res.status(200).json(result)
         })
-    } catch { console.log(error) }
+    } catch { res.status(500).json({ message: "unknown error" }) }
 });
 //#endregion
 
@@ -106,15 +106,16 @@ app.delete('/user', function (req, res) {
         req.body.id = req.body.userIdDelete
         dbMysql.dbMysql.query('DELETE FROM user WHERE id=? ', [req.body.id], function (error, results, fields) {
             if (error) throw error;
-            res.end("OK");
+            res.status(200).json({ message: "utilisateur supprimer" })
         });
-    } catch { console.log(error) }
+    } catch { res.status(500).json({ message: "unknown error" }) }
 });
 //#endregion
 
 // set port, listen for requests
+/*
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 
-});
+});*/
